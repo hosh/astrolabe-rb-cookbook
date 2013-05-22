1a6b8129f39d6fa6969775812f53f20f9c0c8beb
Ho-Sheng Hsiao <hosheng.hsiao@gmail.com>
2013-05-22 11:00:31 -0700

    [ARCH] berks cookbook astrolabe-rb

--- README.md
+++ README.md
@@ -1,4 +1,13 @@
-astrolabe-rb-cookbook
-=====================
+# astrolabe-rb cookbook
 
-Cookbook for setting up and developing Astrolabe.rb
+# Requirements
+
+# Usage
+
+# Attributes
+
+# Recipes
+
+# Author
+
+Author:: YOUR_NAME (<YOUR_EMAIL>)
---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             "astrolabe-rb"
+maintainer       "YOUR_NAME"
+maintainer_email "YOUR_EMAIL"
+license          "All rights reserved"
+description      "Installs/Configures astrolabe-rb"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.1.0"
+
